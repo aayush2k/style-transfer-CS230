c8f6a31b6dec620402d7dd69e12b09259a217dc1
aayush2k <39573479+aayush2k@users.noreply.github.com>
2020-10-30 15:39:29 -0700

    Update style_transfer.py

--- style_transfer.py
+++ style_transfer.py
@@ -2,42 +2,39 @@ import os
 import sys
 import scipy.io
 import scipy.misc
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 from PIL import Image
 from nst_utils import *
-import numpy as np
 import tensorflow as tf
-import pprint
 %matplotlib inline
 
-def compute_content_cost(a_C, a_G):
+def content_cost(a_C, a_G):
     m, n_H, n_W, n_C = a_G.get_shape().as_list()
     a_C_unrolled =  tf.reshape(a_C, shape=[m, n_H * n_W, n_C])
     a_G_unrolled = tf.reshape(a_G, shape=[m, n_H * n_W, n_C])
-    J_content = (1 / (4 * n_H * n_W * n_C)) * tf.reduce_sum(tf.square(tf.subtract(a_C, a_G)))
+    J_content = (1 / (4 * n_H * n_W * n_C)) * tf.reduce_sum(tf.square(tf.subtract(a_C_unrolled, a_G_unrolled)))
     return J_content
 
 def gram_matrix(A):
     GA = tf.matmul(A, tf.transpose(A))    
     return GA
 
-def compute_layer_style_cost(a_S, a_G):
+def layer_style_cost(a_S, a_G):
     m, n_H, n_W, n_C = a_G.get_shape().as_list()
-    a_S = tf.reshape(tf.transpose(a_S), shape=[n_C, n_H * n_W])
-    a_G = tf.reshape(tf.transpose(a_G), shape=[n_C, n_H * n_W])
-    GS = gram_matrix(a_S)
-    GG = gram_matrix(a_G)
+    GS = gram_matrix(tf.reshape(tf.transpose(a_S), shape=[n_C, n_H * n_W]))
+    GG = gram_matrix(tf.reshape(tf.transpose(a_G), shape=[n_C, n_H * n_W]))
     J_style_layer = (1 / (4 * (n_H*n_W)*(n_H*n_W) * n_C*n_C)) * tf.reduce_sum(tf.square(tf.subtract(GS, GG)))
     return J_style_layer
 
-def compute_style_cost(model, STYLE_LAYERS):
+def style_cost(model, STYLE_LAYERS):
     J_style = 0
     for layer_name, coeff in STYLE_LAYERS:
         out = model[layer_name]
         a_S = sess.run(out)
         a_G = out
-        J_style_layer = compute_layer_style_cost(a_S, a_G)
+        J_style_layer = layer_style_cost(a_S, a_G)
         J_style += coeff * J_style_layer
     return J_style
 
@@ -55,9 +52,9 @@ def model_nn(sess, input_image, num_iterations = 200):
         if i%20 == 0:
             Jt, Jc, Js = sess.run([J, J_content, J_style])
             print("Iteration " + str(i) + " :")
-            print("total cost = " + str(Jt))
-            print("content cost = " + str(Jc))
-            print("style cost = " + str(Js))
+            print("Total cost = " + str(Jt))
+            print("Content cost = " + str(Jc))
+            print("Style cost = " + str(Js))
             save_image("output/" + str(i) + ".png", generated_image)
    
     save_image('output/generated_image.jpg', generated_image)
@@ -76,18 +73,16 @@ def main():
     tf.reset_default_graph()
     sess = tf.InteractiveSession()
 
-    content_image = scipy.misc.imread("images/shirt_1.jpg")
-    content_image = reshape_and_normalize_image(content_image)
-    style_image = scipy.misc.imread("images/design_0.jpg")
-    style_image = reshape_and_normalize_image(style_image)
+    content_image = reshape_and_normalize_image(scipy.misc.imread("images/shirt_0.jpg"))
+    style_image = reshape_and_normalize_image(scipy.misc.imread("images/design_1.jpg"))
     model = load_vgg_model("pretrained-model/imagenet-vgg-verydeep-19.mat")
     sess.run(model['input'].assign(content_image))
     out = model['conv4_2']
     a_C = sess.run(out)
     a_G = out
-    J_content = compute_content_cost(a_C, a_G)
+    J_content = content_cost(a_C, a_G)
     sess.run(model['input'].assign(style_image))
-    J_style = compute_style_cost(model, STYLE_LAYERS)
+    J_style = style_cost(model, STYLE_LAYERS)
     J = total_cost(J_content, J_style, alpha = 10, beta = 40)
     optimizer = tf.train.AdamOptimizer(2.0)
     train_step = optimizer.minimize(J)
